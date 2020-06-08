25d0a7fb2cf5a7964081e8fc0dbd4cfca3c56467
José Rocha <zepedro9753@gmail.com>
2020-06-08 04:03:28 +0100

    Started animation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from config import DB_NAME, USER_NAME
+from offsets import get_offsets
 from typing import List, Tuple
 
 import numpy as np
@@ -54,60 +55,40 @@ ax.axis('off')
 ax.set(xlim=(xs_min, xs_max), ylim=(ys_min, ys_max))
 
 cursor_psql = conn.cursor()
-sql = "select distrito,st_union(proj_boundary) from cont_aad_caop2018 group by distrito"
-cursor_psql.execute(sql)
-results = cursor_psql.fetchall()
-xs, ys = [], []
-for row in results:
-    geom = row[1]
-    if type(geom) is MultiPolygon:
-        for pol in geom:
-            xys = pol[0].coords
-            xs, ys = [], []
-            for (x, y) in xys:
-                xs.append(x)
-                ys.append(y)
-            ax.plot(xs, ys, color='black', lw='0.2')
-    if type(geom) is Polygon:
-        xys = geom[0].coords
-        xs, ys = [], []
-        for (x, y) in xys:
-            xs.append(x)
-            ys.append(y)
-        ax.plot(xs, ys, color='black', lw='0.2')
+get_offsets(cursor_psql)
+# sql = "select distrito,st_union(proj_boundary) from cont_aad_caop2018 group by distrito"
+# cursor_psql.execute(sql)
+# results = cursor_psql.fetchall()
+# xs, ys = [], []
+# for row in results:
+#     geom = row[1]
+#     if type(geom) is MultiPolygon:
+#         for pol in geom:
+#             xys = pol[0].coords
+#             xs, ys = [], []
+#             for (x, y) in xys:
+#                 xs.append(x)
+#                 ys.append(y)
+#             ax.plot(xs, ys, color='black', lw='0.2')
+#     if type(geom) is Polygon:
+#         xys = geom[0].coords
+#         xs, ys = [], []
+#         for (x, y) in xys:
+#             xs.append(x)
+#             ys.append(y)
+#         ax.plot(xs, ys, color='black', lw='0.2')
 
 
-sql = """select st_astext(proj_track),ts,taxi from tracks where taxi='20000333' order by ts"""
-cursor_psql.execute(sql)
-results = cursor_psql.fetchall()
+# offsets = get_offsets()
+
 # x, y = [], []
-results = [[linestring_to_points(i[0]), i[1]] for i in results]
-ts_i = results[0][1]
-offsets = [[] for _ in results]
-for i, row in enumerate(results):
-    diff = row[1] - ts_i
-    start_fillers = [[float(0), float(0)] for _ in range(diff)]
-    offsets[i].append(start_fillers)
-    offsets[i].append(row[0])
+# for i in offsets[0]:
+#     x.append(i[0])
+#     y.append(i[1])
 
-max_depth = len(max(offsets, key=lambda x: len(x)))
-for i in offsets:
-    diff = max_depth - len(i)
-    end_fillers = [[float(0), float(0)] for _ in range(diff)]
-    i.append(end_fillers)
+# scat = ax.scatter(x, y, s=2, color='orange')
+# anim = FuncAnimation(
+#     fig, animate, interval=10, frames=len(offsets)-1, repeat=False)
 
-offsets = list(map(list, zip(*offsets)))  # invert matrix
-
-print(offsets[0][0])
-
-x, y = [], []
-for i in offsets[0]:
-    x.append(i[0])
-    y.append(i[1])
-
-scat = ax.scatter(x, y, s=2, color='orange')
-anim = FuncAnimation(
-    fig, animate, interval=10, frames=len(offsets)-1, repeat=False)
-
-plt.draw()
-plt.show()
+# plt.draw()
+# plt.show()
